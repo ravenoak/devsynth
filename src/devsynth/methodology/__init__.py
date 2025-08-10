c9d7b000c388defd885dd1fb8e3ecbab71383ff7
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2025-08-09 17:26:34 -0700

    Expose reasoning loop and enhance consensus logging

--- src/devsynth/methodology/__init__.py
+++ src/devsynth/methodology/__init__.py
@@ -1,11 +1,13 @@
 from .adhoc import AdHocAdapter
-from .sprint import SprintAdapter
+from .dialectical_reasoning import reasoning_loop
 from .kanban import KanbanAdapter
 from .milestone import MilestoneAdapter
+from .sprint import SprintAdapter
 
 __all__ = [
     "AdHocAdapter",
     "SprintAdapter",
     "KanbanAdapter",
     "MilestoneAdapter",
+    "reasoning_loop",
 ]
