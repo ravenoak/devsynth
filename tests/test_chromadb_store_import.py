9d7d186857980e5e219d645c286da011bea29332
Caitlyn O'Hanna <caitlyn.ohanna@gmail.com>
2025-08-18 15:41:17 -0700

    test: mark chromadb import test as fast

--- tests/test_chromadb_store_import.py
+++ tests/test_chromadb_store_import.py
@@ -4,7 +4,11 @@ pytest.importorskip("chromadb")
 from tests.lightweight_imports import apply_lightweight_imports
 
 
+@pytest.mark.fast
 def test_chromadb_store_import() -> None:
-    """ChromaDBStore should import when chromadb extras are installed."""
+    """ChromaDBStore should import when chromadb extras are installed.
+
+    ReqID: N/A
+    """
     apply_lightweight_imports()
     from devsynth.application.memory.chromadb_store import ChromaDBStore  # noqa: F401
